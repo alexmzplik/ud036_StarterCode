f6716d9e4cf61aef156859936d2f0a9ebfda603b
Alexandre Vaz <mecontrate@alexandrevaz.site>
2017-12-18 04:00:53 -0200

    fazendo a classe de mídia mais genérica para uso na classe poster

--- lib/media.py
+++ lib/media.py
@@ -1,14 +1,26 @@
 # -*- coding: UTF-8 -*-
+from datetime import datetime
+from lib.validation import Validate
 
 class Media(object):
     def __init__(self, title, sinopse, year=None):
         self._title = title
         self._sinopse = sinopse
         self._year = year
+        self._inserted_date = datetime.now()
+        self._last_change = datetime.now()
 
     @property
     def title(self):
         return self._title
+
+    @property
+    def name(self):
+        return self._title
+
+    @property
+    def description(self):
+        return self._sinopse
 
     @property
     def sinopse(self):
@@ -17,3 +29,21 @@ class Media(object):
     @property
     def year(self):
         return self._year
+
+    @year.setter
+    def year(self, value):
+        if Validate.is_valid_year(value):
+            self._year = value
+
+    @property
+    def last_change(self):
+        return self._last_change
+
+    @property
+    def created(self):
+        return self._inserted_date
+    
+    @created.setter
+    def created(self, value):
+        if Validate.is_valid_date(value):
+            self._inserted_date = value
