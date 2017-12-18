bf411c8023d9c3b671b8b1e601620d46a2fd62f1
Alexandre Vaz <mecontrate@alexandrevaz.site>
2017-12-18 04:16:09 -0200

    Revert "usando a classe de lista de posters"
    This reverts commit d3148b4aaa55806a336c19ecabc150686891d964.

--- lib/movie.py
+++ lib/movie.py
@@ -1,12 +1,11 @@
 # -*- coding: UTF-8 -*-
 from lib.media import Media
 from lib.trailers import Trailers
-from lib.posters import Posters
 
 class Movie(Media):
     def __init__(self, title, sinopse, year=None):
         super(Movie, self).__init__(title, sinopse, year)
-        self._posters = Posters()
+        self._posters = []
         self._trailers = Trailers()
     
     def __repr__(self):
@@ -16,21 +15,16 @@ class Movie(Media):
             "year": self.year
         })
     
-    def add_poster(self, name, url, description = None):
-        self._posters.add(name, url, description)
+    def add_poster(self, url, description = None):
+        pass
 
     def add_trailer(self, name, url, isFeatured=False):
         ''' adds a trailer to the trailer list '''
         self._trailers.add(name, url, isFeatured)
     
     @property
-    def posters(self):
-        ''' list all posters '''
-        return self._posters
-
-    @property
     def trailers(self):
-        ''' list all trailers '''
+        ''' list all trailers from the list '''
         return self._trailers.list
 
     @property
