511598e9418d72aab4a41a83fe35a6739069fe69
Alexandre Vaz <mecontrate@alexandrevaz.site>
2017-12-18 04:19:58 -0200

    removendo métodos que passam a ser da classe de mídia

--- lib/trailer.py
+++ lib/trailer.py
@@ -1,52 +1,36 @@
 # -*- coding: UTF-8 -*-
-from validation import Validate
-from datetime import datetime
+from lib.validation import Validate
+from lib.media import Media
 
-class Trailer(object):
-    def __init__(self, title, url, date_release = None):
-        self._title = title
-        self._url = None
-        self._released = date_release
-
-        self.url = url
-        self._inserted_date = datetime.now()
-        self.__has_changed()
+class Trailer(Media):
+    def __init__(self, title, url, release_date = None):
+        super(Trailer, self).__init__(title, None)
+        self._url = url
+        self._released = release_date
 
     def __repr__(self):
         return 'Trailer({!r})'.format({
             "title": self.title,
             "url": self.url,
+            "release_date": self.release_date,
+            "created_date": self.created,
             "last_change": self.last_change
         })
-
-    def __has_changed(self):
-        self._last_change = datetime.now()
-    
-    @property
-    def last_change(self):
-        return self._last_change#.isoformat()
-
-    @property
-    def title(self):
-        return self._title
-    @title.setter
-    def title(self, value):
-        self._title = value
 
     @property
     def url(self):
         return self._url
+
     @url.setter
     def url(self, value):
-        if Validate.is_valid_url(value):            
+        if Validate.is_valid_url(value):           
             self._url = value
 
     @property
-    def date_release(self):
+    def release_date(self):
         ''' Retun the date when the trailer was released'''
         return self._released
 
-    @property
-    def created(self):
-        ''' Retun the date when the trailer was registered'''
-        return self._inserted_date
+    @release_date.setter
+    def release_date(self, value):
+        self._released = value
