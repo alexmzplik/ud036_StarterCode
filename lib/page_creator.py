b2503ab9d1aaa6e24d9fc96d64a619ba08a0c7d7
Alexandre Vaz <mecontrate@alexandrevaz.site>
2017-12-19 18:30:48 -0200

    change of local for page_creator.py

--- lib/page_creator.py
+++ lib/page_creator.py
@@ -4,12 +4,22 @@ from os.path import abspath as AbsolutePath
 from lib.movies import Movies
 
 def youtube_id_from_trailer(url):
+    """Extrai o id do vídeo no youtube
+
+    Argumentos-chave:
+    url -- link para o vídeo
+    """
     from re import search as regex_search
     result = regex_search(r'(?<=v=)[^&#]+', url)
     result = result or regex_search(r'(?<=be/)[^&#]+', url)
     return result.group(0) if result else None
 
 def create_movie_tiles_content(movies):
+    """Popula o bloco dos filmes para a página
+
+    Argumentos-chave:
+    movies -- lista com filmes criada com a classe Movie
+    """
     movie_tile_content = open('model/movie_tile.html', 'r').read()
     content = ''
     for movie in movies:
@@ -21,18 +31,19 @@ def create_movie_tiles_content(movies):
         )
     return content
 
-def open_movies_page(movies):
+def open_movies_page(movies, html_page):
+    """Cria a página de filmes a a abre no browser
+
+    Argumentos-chave:
+    movies -- lista com filmes criada com a classe Movie
+    """
+    output_file = open(html_page+ '.html', 'w')
     header = open('model/header.html', 'r').read()
     body = open('model/body.html', 'r').read()
-    output_file = open('fresh_tomatoes.html', 'w')
+    footer = open('model/footer.html', 'r').read()
     rendered_content = body.format(movie_tiles=create_movie_tiles_content(movies))
     
-    output_file.write(header + rendered_content)
+    output_file.write(header + rendered_content + footer)
     output_file.close()
     url = AbsolutePath(output_file.name)
     OpenBrowser('file://' + url, new=2)
-
-shelf = Movies()
-movies_json = 'data/movies.json'
-shelf.read_json(movies_json)
-open_movies_page(shelf.items)
