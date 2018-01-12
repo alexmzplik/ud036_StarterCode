3d10c199395f10f4c9909b326fd31cd63708ea7a
Alexandre Vaz <mecontrate@alexandrevaz.site>
2018-01-12 16:04:02 -0200

    Some documentation for the file

--- lib/media.py
+++ lib/media.py
@@ -3,6 +3,19 @@ from datetime import datetime
 from lib.validation import Validate
 
 class Media(object):
+    """ Class base for any type of media like movie, poster, trailer, book, etc.
+        Arguments:
+          title (str): the title/name for the media.
+          sinopse (str): the description of the media.
+          year (int): year when the media was published or released, optional.
+        Properties:
+            title (str): the title/name of the media.
+            name (str): a shortcut to the title of the media.
+            sinopse (str): the description of the media.
+            year (int): year when the media was published or released.
+            created (datetime): the date when the media object was created.
+            last_change (datetime): the date of the last change of the media.
+    """
     def __init__(self, title, sinopse, year=None):
         self._title = title
         self._sinopse = sinopse
